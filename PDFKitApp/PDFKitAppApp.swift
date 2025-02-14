a3deffe04c4933521c7a23b15175bbc91a13a710
PortableDoughnut <gwen.thelin@gmail.com>
2025-02-14 09:33:11 -0700

    Initial Commit

---
+++ PDFKitApp/PDFKitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFKitAppApp.swift
+//  PDFKitApp
+//
+//  Created by Gwen Thelin on 2/14/25.
+//
+
+import SwiftUI
+
+@main
+struct PDFKitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
